558bbd15410a8cdba8eac54fd05382dccb444bea
jameslinnell <jimlinnell@gmail.com>
2024-12-01 20:38:41 +0000

    init script

---
+++ init.sh
@@ -0,0 +1,3 @@
+sudo apt update
+sudo apt upgrade
+sudo apt install nordvpn make awscli curl poetry wget pyenv build-essential ubuntu-restricted-extras btop ncdu tldr atuin exa bat fzf neofetch neovim git pip i3 polybar feh rofi wireplumber dconf-editor dunst picom glow kitty alacritty python3-dev python3-pip python3-setuptools distutils
