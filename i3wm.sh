74da3009cf2f1e0e3c4569b397525190b386cef7
jameslinnell <jimlinnell@gmail.com>
2024-12-03 11:52:12 +0000

    Add an xfce4 install script

---
+++ i3wm.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+function i3setup() {
+  echo "Setting up i3..."
+  sudo apt install i3 polybar feh rofi wireplumber dunst picom -y
+  current_dir=$(pwd)
+  mkdir ~/.config/i3
+  cp -rf $current_dir/config/i3/* ~/.config/i3/
+  cp -rf $current_dir/config/polybar ~/.config/
+  echo "i3 window manger is now installed. Log out, and on the login screen set i3 to be the window manager before logging back in." 
+}
+
+function prompt_user() {
+  local prompt_message="$1"
+  local function_to_run="$2"
+  
+  while true; do
+    read -p "$prompt_message (Y/n): " user_input
+    case $user_input in
+      [Yy]*|"") # Default to 'yes' if the user presses enter without typing anything
+        $function_to_run
+        break
+        ;;
+      [Nn]*)
+        echo "Skipping $function_to_run."
+        break
+        ;;
+      *)
+        echo "Please answer Y or n."
+        ;;
+    esac
+  done
+}
+
+# Main script execution with prompts
+prompt_user "Would you like to set up i3?" i3setup
---
+++ xfce4.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+function xfce4setup() {
+  echo "Setting up xfce4..."
+  sudo apt install xfce4 -y
+  # current_dir=$(pwd)
+  # mkdir ~/.config/i3
+  # cp -rf $current_dir/config/i3/* ~/.config/i3/
+  # cp -rf $current_dir/config/polybar ~/.config/
+  echo "xfce4 window manger is now installed. Log out, and on the login screen set Xfce Session to be the window manager before logging back in." 
+}
+
+function prompt_user() {
+  local prompt_message="$1"
+  local function_to_run="$2"
+  
+  while true; do
+    read -p "$prompt_message (Y/n): " user_input
+    case $user_input in
+      [Yy]*|"") # Default to 'yes' if the user presses enter without typing anything
+        $function_to_run
+        break
+        ;;
+      [Nn]*)
+        echo "Skipping $function_to_run."
+        break
+        ;;
+      *)
+        echo "Please answer Y or n."
+        ;;
+    esac
+  done
+}
+
+# Main script execution with prompts
+prompt_user "Would you like to set up i3?" xfce4setup
